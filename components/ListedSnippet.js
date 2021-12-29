fc1f2f28873807b0b185aa0ba1e1aab7089b23c3
ShortyDev <jakob123413@gmail.com>
2021-12-29 01:46:43 +0100

    implemented features

--- components/ListedSnippet.js
+++ components/ListedSnippet.js
@@ -1,9 +1,23 @@
 import styles from '../styles/ListedSnippet.module.scss'
 
-const ListedSnippet = ({children, ...props}) => {
+const ListedSnippet = ({...props}) => {
+  let tags = props.tags.map((tag, index) => {
+    return (
+      <span key={index} className={styles.tag}>{tag}</span>
+    )
+  })
   return (
-    <div className={styles.ListedSnippet} {...props}>
-      Test {props}
+    <div className={styles["listed-snippet"]} {...props}>
+      {tags}
+      <span className={styles["span-left"]}>{props.title}</span>
+      <span className={styles["span-right"]}>{`${props.author} // ${props.language}`}</span>
+      <div name={"snippet"} className={styles.code}>
+        <pre>
+          <code className={"java"}>
+            {props.code}
+          </code>
+        </pre>
+      </div>
     </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import 'highlight.js/styles/atom-one-dark.css';
+import ListedSnippet from "../components/ListedSnippet";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
