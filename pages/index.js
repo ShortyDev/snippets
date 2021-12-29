9bd0b9e6b01d88a730df1a413ca4c2e64cc69ebf
ShortyDev <jakob123413@gmail.com>
2021-12-29 16:35:35 +0100

    improved search

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,10 @@ import hljs from 'highlight.js';
 export default function Home({container}) {
   let loadedSnippets = [];
   JSON.parse(container).reverse().forEach(snippet => {
-    loadedSnippets.push(<ListedSnippet key={snippet.props.title} title={snippet.props.title} author={snippet.props.author} language={snippet.props.language} snippetId={snippet.props.snippetId} tags={snippet.props.tags} code={snippet.props.code}/>)
+    loadedSnippets.push(<ListedSnippet key={snippet.props.title} title={snippet.props.title}
+                                       author={snippet.props.author} language={snippet.props.language}
+                                       snippetId={snippet.props.snippetId} tags={snippet.props.tags}
+                                       code={snippet.props.code}/>)
   });
   useEffect(() => {
     const search = document.getElementById("search");
@@ -15,25 +18,53 @@ export default function Home({container}) {
     const inputHandler = (e) => {
       let searchTerm = e.target.value.toLowerCase();
       for (let i = 0; i < snippets.length; i++) {
+        snippets[i].style.display = "none";
         if (searchTerm.length === 0) {
           snippets[i].style.display = "block";
         }
         searchTerm.split(" ").forEach((term) => {
-          if (term.length === 0) return;
+          if (term.length === 0) {
+            return;
+          }
           let block = snippets.item(i).attributes.codeblock
           let title = snippets.item(i).attributes.getNamedItem("title").value.toLowerCase();
           let author = snippets.item(i).attributes.getNamedItem("author").value.toLowerCase();
           let language = snippets.item(i).attributes.getNamedItem("language").value.toLowerCase();
           let tags = snippets.item(i).attributes.getNamedItem("tags").value.toLowerCase();
           let code = block !== undefined ? block.toLowerCase() : "";
-          if (title.includes(term) || author.includes(term) || language.includes(term) || tags.includes(term) || code.includes(term))
+          title.split(" ").forEach((word) => {
+            if (word.length === 0) {
+              snippets[i].style.display = "block";
+              return;
+            }
+            if (word === term) {
+              snippets[i].style.display = "block";
+            }
+          });
+          if (author.includes(term) || language.includes(term) || tags.includes(term))
             snippets[i].style.display = "block";
-          else
-            snippets[i].style.display = "none";
+          if (term.includes(".") && code.includes(term))
+            snippets[i].style.display = "block"
+          code.split(" ").forEach((word) => {
+            if (word.length === 0) return
+            if (word.includes("public") || word.includes("void") || word.includes("private") || word.includes("protected") || word.includes("static")) return
+              word.split(".").forEach((word) => {
+                if (word.length === 0) return
+                word.split("(").forEach((word) => {
+                  if (word.length === 0) return
+                  if (word === term)
+                    snippets[i].style.display = "block"
+                })
+              })
+          })
         })
       }
     }
     search.addEventListener('input', inputHandler)
+    document.getElementById("title").addEventListener('click', (e) => {
+      search.value = ""
+      inputHandler({target: {value: ""}})
+    })
     for (let i = 0; i < snippets.length; i++) {
       snippets.item(i).firstChild.addEventListener('click', () => {
         let open = snippets.item(i).attributes.open;
@@ -53,7 +84,7 @@ export default function Home({container}) {
 
       <main>
         <div className={styles["top-bar"]}>
-          <h1 className={styles["title"]}><span className={styles["title-small"]}>shorty // </span>snippets</h1>
+          <h1 id={"title"} className={styles["title"]}><span className={styles["title-small"]}>shorty // </span>snippets</h1>
           <div>
             <input id="search" className={styles["search"]} type="search" placeholder={"search here..."}/>
           </div>
@@ -65,9 +96,6 @@ export default function Home({container}) {
     </div>
   )
 }
-
-const environment = "production";
-const path = environment === "production" ? "https://snippets.shortydev.eu/" : "http://localhost:3000/";
 
 import snippets from "../components/snippets/desc.json";
 
