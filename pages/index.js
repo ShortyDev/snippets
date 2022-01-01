f6b1541b3dbc2487daa587d58a3722bb0372a56c
ShortyDev <jakob123413@gmail.com>
2022-01-01 03:17:44 +0100

    toggler coming soon

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import styles from '../styles/index.module.scss'
 import ListedSnippet from "../components/ListedSnippet";
 import {useEffect} from "react";
-import hljs from 'highlight.js';
 
 export default function Home({container}) {
   let loadedSnippets = [];
@@ -48,14 +47,14 @@ export default function Home({container}) {
           code.split(" ").forEach((word) => {
             if (word.length === 0) return
             if (word.includes("public") || word.includes("void") || word.includes("private") || word.includes("protected") || word.includes("static")) return
-              word.split(".").forEach((word) => {
+            word.split(".").forEach((word) => {
+              if (word.length === 0) return
+              word.split("(").forEach((word) => {
                 if (word.length === 0) return
-                word.split("(").forEach((word) => {
-                  if (word.length === 0) return
-                  if (word === term)
-                    snippets[i].style.display = "block"
-                })
+                if (word === term)
+                  snippets[i].style.display = "block"
               })
+            })
           })
         })
       }
@@ -73,6 +72,20 @@ export default function Home({container}) {
         snippets.item(i).children.namedItem("snippet").style.display = open ? "block" : "none";
       })
     }
+  /*  let toggler = document.getElementById("toggler");
+    toggler.addEventListener('click', (e) => {
+      if (localStorage.getItem("dark") === "true") {
+        localStorage.setItem("dark", "false")
+      } else {
+        localStorage.setItem("dark", "true")
+      }
+      toggler.innerHTML = localStorage.getItem("dark") !== "true" ? (
+          `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-sun"><circle cx="12" cy="12" r="5"/><line x1="12" y1="1" x2="12" y2="3"/><line x1="12" y1="21" x2="12" y2="23"/><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"/><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"/><line x1="1" y1="12" x2="3" y2="12"/><line x1="21" y1="12" x2="23" y2="12"/><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"/><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"/></svg>`) :
+        (`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/></svg>`)
+    })
+    toggler.innerHTML = localStorage.getItem("dark") !== "true" ? (
+        `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-sun"><circle cx="12" cy="12" r="5"/><line x1="12" y1="1" x2="12" y2="3"/><line x1="12" y1="21" x2="12" y2="23"/><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"/><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"/><line x1="1" y1="12" x2="3" y2="12"/><line x1="21" y1="12" x2="23" y2="12"/><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"/><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"/></svg>`) :
+      (`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/></svg>`)*/
   })
   return (
     <div>
@@ -84,9 +97,12 @@ export default function Home({container}) {
 
       <main>
         <div className={styles["top-bar"]}>
-          <h1 id={"title"} className={styles["title"]}><span className={styles["title-small"]}>shorty // </span>snippets</h1>
+          <h1 id={"title"} className={styles["title"]}><span className={styles["title-small"]}>shorty // </span>snippets
+          </h1>
           <div>
             <input id="search" className={styles["search"]} type="search" placeholder={"search here..."}/>
+          </div>
+          <div className={styles["darkModeToggle"]} id={"toggler"}>
           </div>
         </div>
         <div id="snippets" className={styles["snippet-list"]}>
